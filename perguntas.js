5d9ca405cdad04e7466f124c8550eb8cdfc43586
MariaHaenisch <140609456+MariaHaenisch@users.noreply.github.com>
2024-10-29 08:38:24 -0300

    29.10

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'História',
+    'Em que ano ocorreu a primeira guerra mundial?',
+    '1914 a 1918'
+)
+criaCartao(
+    'História',
+    'Onde ocorreu a Guerra de Canudos?',
+    'No sertão nordestino'
+)
+criaCartao(
+    'História',
+    'Quem era o líder religioso da Guerra de Canudos?',
+    'João Antonio'
+)
+criaCartao(
+    'Matemática',
+    'Qual é o nome do maior lado do triângulo retângulo?',
+    'Hipotenusa'
+)
+criaCartao(
+    'Sociologia',
+    'Segundo Émile Durkheim (1858-1917), as três principais características do Fato Social são:',
+    'A generalidade, a exterioridade e a coercitividade.'
+)
+criaCartao(
+    'Biologia',
+    'Pares de genes, com segregação independente podem agir, conjuntamente, na determinação de uma mesma característica fenotípica. Este fenômeno é conhecido como:',
+    'nteração gênica'
+)
+criaCartao(
+    'Português',
+    'A palavra "anistia" tem relação com:',
+    'Perdão'
+)
+criaCartao(
+    'Português',
+    'Uma pessoa altruísta é aquela que...',
+    'gosta de ajudar, é generosa'
+)
+criaCartao(
+    'Português',
+    'Leniência é uma palavra que significa...',
+    'Agradável'
+)
+criaCartao(
+    'Sociologia',
+    'É a condição material dos indivíduos que determinaria os demais aspectos de sua vida. A importância dada por Marx a esse quesito de nossas vidas é justificada, segundo sua teoria, em razão do impacto que a situação econômica de um sujeito tem em sua trajetória de formação. Essa linha de pensamento é chamada de:',
+    'Materialismo Histórico'
+)
+criaCartao(
+    'Sociologia',
+    'Para Marx, o capital era o principal ponto a ser investigado para que fosse possível entender as mudanças sociais que surgem em um dado momento. Entende-se por capital:',
+    'Qualquer bem que possa ser investido para gerar mais lucro'
+)
+criaCartao(
+    'Biologia',
+    'A produção de insulina pela técnica do DNA recombinante tem impacto favorável na saúde de indivíduos:',
+    'Diabéticos'
+)
+criaCartao(
+    'Programação',
+    'O que e JavaScrip',
+    'O Javascript é uma linguagem de programação'
+)
+criaCartao(
+    'Educação Financeira',
+    'Qual é a moeda da indonésia?',
+    'Rupia indonésia<'
+)
+
