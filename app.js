1b91c960a3a47241c8bfae193760b5bd9b2f3886
MariaHaenisch <140609456+MariaHaenisch@users.noreply.github.com>
2024-11-05 08:18:10 -0300

    da

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 function criaCartao(categoria, pergunta, resposta){
-    console.log(categoria, pergunta, resposta);
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML = `
+   <div class="Cartao__Conteudo">
+                    <h3>${categoria}</h3>
+                    <div class="Cartao_Conteudo_Pergunta">
+                        <p>${pergunta}</p>
+                    </div>
+                    <div class="Cartao__Conteudo_Resposta">
+                        <p>${resposta}</p>
+                    </div>
+                </div>
+    `
+    let container = document.getElementById('container')
+    container.appendChild(cartao)
 }
